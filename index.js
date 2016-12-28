e45cfd2400c0659d37ae04ee09c4490148e70b7f
Blake Guilloud <fbguillo@gmail.com>
2016-12-28 07:51:46 -0500

    Migrated file structure

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
 const commander = require('commander');
-const rcc = require('./rcc').createFile;
+const rcc = require('./rcc');
 
 commander.version('0.1.5');
 commander.command('component [value] [value]')
   .description('creates a templated React Component')
   .action((name, path) => {
-    rcc(name, 'components', path);
+    rcc.createFile(name, 'components', path);
   });
 
 commander.command('pure [value] [value]')
   .description('creates a templated React pure function')
   .action((name, path) => {
-    rcc(name, 'pures', path);
+    rcc.createFile(name, 'pures', path);
   });
 
 commander.command('container [value] [value]')
   .description('creates a React component with connect mapping')
   .action((name, path) => {
-    rcc(name, 'containers', path);
+    rcc.createFile(name, 'containers', path);
   });
 
 commander.parse(process.argv);
