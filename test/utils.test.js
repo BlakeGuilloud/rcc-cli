f4e5aa21ac3572f43693ea90303620e972ec40c4
Blake Guilloud <blake.guilloud@gmail.com>
2017-11-12 20:07:50 -0500

    tests for replace.

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,9 +1,21 @@
 const {
+  replace,
   getFileExtension,
   getFileDest,
   getTemplate,
   getFilePath,
 } = require('../rcc/utils');
+
+describe('replace', () => {
+  it('should replace keywords in a string with provided text', () => {
+    expect(replace('the dog will eat', 'dog', 'cat'))
+      .toBe('the cat will eat');
+    expect(replace('const <#fileName> = () =>', '<#fileName>', 'Header'))
+      .toBe('const Header = () =>');
+    expect(replace('class <#fileName> extends Component', '<#fileName>', 'HeaderContainer'))
+      .toBe('class HeaderContainer extends Component');
+  });
+});
 
 describe('getFileExtension', () => {
   it('should map file extension', () => {
@@ -19,7 +31,7 @@ describe('getFileDest', () => {
   it('should map file destination', () => {
     expect(getFileDest('foo')).toBe(`${process.cwd()}/foo`);
     expect(getFileDest('bar')).toBe(`${process.cwd()}/bar`);
-    expect(getFileDest()).toBe(`${process.cwd()}/`);
+    expect(getFileDest()).toBe(process.cwd());
   });
 });
 
